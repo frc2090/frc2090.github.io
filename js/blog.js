d91aece333fc9ef4f17e4fe622bddd457a04b9d3
dchotzen-hartzell <dchotzen-hartzell19@punahou.edu>
2017-01-16 14:05:43 -1000

    Initial Commit

---
+++ js/blog.js
@@ -0,0 +1,33 @@
+
+//http://devinstestblog2090.blogspot.com/feeds/posts/default
+var url = 'http://punahoufrc2090.blogspot.com/feeds/posts/default';
+var div = document.getElementById("blog-div");
+feednami.load(url)
+  .then(feed => {
+    for (var k = 0; k < feed.entries.length; k++) {
+      var entry = feed.entries[k];
+      displayEntry(entry);
+    }
+})
+
+function displayEntry(entry) {
+  var container = document.createElement("div");
+  container.className = "blogpost"
+  var title = document.createElement("b");
+  title.className = "blog-title";
+  title.innerHTML = entry.title;
+  container.appendChild(title);
+  container.appendChild(document.createElement("br"));
+  var author = document.createElement("b");
+  author.className = "blog-authorline";
+  author.innerHTML = "By " + entry.author;
+  container.appendChild(author);
+  container.appendChild(document.createElement("hr"));
+  var text = document.createElement("p");
+  text.innerHTML = entry.description;
+  container.appendChild(text);
+  document.getElementById("page-content").appendChild(container);
+  document.getElementById("loading").innerHTML = "";
+
+
+}
