08bef316812d81f3b00ce0a6f4482c6740046eea
dchotzen-hartzell <dchotzen-hartzell19@punahou.edu>
2017-02-06 16:04:30 -1000

    Test

---
+++ js/videos.js
@@ -0,0 +1,22 @@
+//https://www.youtube.com/feeds/videos.xml?playlist_id=PLm7Fmr93fupPfPDZ-_ZJDZ1gQvW16W-gZ
+viewVid("https://www.youtube.com/feeds/videos.xml?playlist_id=PLm7Fmr93fupPfPDZ-_ZJDZ1gQvW16W-gZ");
+//<iframe width="560" height="315" src="https://www.youtube.com/embed/p33FsCkoBko" frameborder="0" allowfullscreen></iframe>
+function viewVid(url) {
+  feednami.setPublicApiKey('6e18b2484f28c3f1dff3223b466f38f0e24bb9a0ab53f0581f626354496f1450');
+  feednami.load(url)
+    .then(feed => {
+      for (var k = 0; k < feed.entries.length; k++) {
+        var entry = feed.entries[k];
+        displayEntry(entry);
+      }
+  })
+  document.getElementById("loading").innerHTML = "";
+}
+
+function displayEntry(entry) {
+  var e = document.createElement("iframe");
+  e.src = entry.link.replace('watch?v=', 'embed/');
+  e.frameborder = '0';
+  e.setAttribute('allowFullScreen', '');
+  document.getElementById("page-content").appendChild(e);
+}
